6da1b5446882594883c8a1f03bd9d205090df0ce
Aleksandr Semenov <semenlinux@gmail.com>
2021-05-24 16:10:34 +0300

    Add testing flat function

---
+++ tests/test_flat.py
@@ -0,0 +1,19 @@
+from code import flat
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "some_list, list_result, exception",
+    [
+        ([[1, 2], [3]], [1, 2, 3], None),
+        ([[1, 2], [3,[4]]], [1, 2, 3,[4]], None),
+        ([1, 2, 3], [], TypeError),
+    ],
+)
+def test_flat(some_list, list_result, exception):
+    if not exception:
+        assert flat(some_list) == list_result
+    else:
+        with pytest.raises(exception):
+            assert flat(some_list) == list_result
