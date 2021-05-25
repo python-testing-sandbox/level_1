25cc345bf5d03886177259cf07c754b0548a9d00
Aleksandr Semenov <semenlinux@gmail.com>
2021-05-25 14:40:57 +0300

    Add test parse iso datetime

---
+++ tests/test_parse_iso_datetime.py
@@ -0,0 +1,17 @@
+import datetime
+import pytest
+
+from code import parse_iso_datetime
+
+
+@pytest.mark.parametrize(
+    "iso_datetime, result",
+    [
+        ("2020-10-22T14:29:38", datetime.datetime(2020,10,22,14,29,38)),
+        ("2019-09-21T13:18:27Z", datetime.datetime(2019,9,21,13,18,27)),
+        ("2019.09.21 13:18:27", None),
+        ("string", None),
+    ],
+)
+def test_parse_iso_datetime(iso_datetime, result):
+    assert parse_iso_datetime(iso_datetime) == result
