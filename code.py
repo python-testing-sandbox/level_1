e96932d152919a3957b33870008b4a21709a6f3a
andimeon-ubuntu <andimeon@gmail.com>
2021-05-27 17:31:17 +0900

    Fix flake8 bugs

--- code.py
+++ code.py
@@ -107,4 +107,3 @@ def max_with_default(items: Iterable, default: Optional = None):
 
 def is_python_class_name(name: str) -> bool:
     return name[0] == name[0].upper() and name[1:] == name[1:].lower()
-
